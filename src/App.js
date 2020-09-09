5acfe32441a19a62b0b320936c7e41f01152e698
Raul T <jr.torsa@gmail.com>
2020-09-09 11:51:59 -0600

    added cancion component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import axios from "axios";
 
 function App() {
   // definir letra
   const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState("");
 
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
 
     const consultarApiLetra = async () => {
-      const { artista, letra } = busquedaletra;
-      const url = `https://api.lyrics.ovh/v1/${artista}/${letra}`;
+      const { artista, cancion } = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+      guardarLetra(resultado.data.lyrics);
     };
     consultarApiLetra();
   }, [busquedaletra]);
@@ -18,6 +24,13 @@ function App() {
   return (
     <Fragment>
       <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
